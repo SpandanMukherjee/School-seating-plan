e29e0e49d8bb85bda0255ed49fc09fa762da7cf1
SpandanMukherjee <spandan.mukherjee428@gmail.com>
2025-09-20 18:57:02 +0530

    working input (CSV TO SQL)

---
+++ src/csv_import.py
@@ -0,0 +1,79 @@
+import csv
+import mysql.connector
+import getpass
+
+try:
+    password = getpass.getpass("Enter password: ")
+
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password=password,
+        database="School_seating_plan"
+    )
+
+    cursor = conn.cursor()
+
+    if conn.is_connected():
+        print("Successfully connected to the database")
+
+    cursor.execute("""
+                   
+        CREATE TABLE IF NOT EXISTS classes(
+            class VARCHAR(5) NOT NULL,
+            section VARCHAR(5) NOT NULL,
+            boys INT,
+            girls INT,
+            total INT NOT NULL,
+            PRIMARY KEY (class, section)
+        );
+                   
+    """)
+
+    cursor.execute("""
+                   
+        CREATE TABLE IF NOT EXISTS exam_fixture(
+            exam_date DATE PRIMARY KEY,
+            VI VARCHAR(100),
+            VII VARCHAR(100),
+            VIII VARCHAR(100),
+            IX VARCHAR(100),
+            X VARCHAR(100),
+            XI VARCHAR(100),
+            XII VARCHAR(100)
+        );
+                   
+    """)
+
+    cursor.execute("""
+                   
+        CREATE TABLE IF NOT EXISTS rooms(
+            room_no VARCHAR(20) PRIMARY KEY,
+            capacity INT NOT NULL
+        );
+                   
+    """)
+
+    def import_csv_to_table(filename, table_name, columns):
+        cursor.execute(f"TRUNCATE TABLE {table_name}")
+
+        with open(filename, "r") as file:
+            reader = csv.reader(file)
+            next(reader)
+
+            for row in reader:
+                placeholders = ', '.join(['%s'] * len(columns))
+                sql = f"INSERT INTO {table_name} ({','.join(columns)}) VALUES ({placeholders})"
+                cursor.execute(sql, row)
+
+    import_csv_to_table("data/classes.csv", "classes", ["class", "section", "boys", "girls", "total"])
+    import_csv_to_table("data/exam_fixture.csv", "exam_fixture", ["exam_date", "VI", "VII", "VIII", "IX", "X", "XI", "XII"])
+    import_csv_to_table("data/rooms.csv", "rooms", ["room_no", "capacity"])
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+    print("Data imported successfully")
+
+except Exception as e:
+    print("Error while connecting to the database:", e)
